893bfbba45c3c9540bfeb909a927573f60cd938d
MarcinosP <aaarcinm@gmail.com>
2023-04-14 23:37:16 +0200

    feat: changes in tensorflow prediction

--- tensorflow_task.py
+++ tensorflow_task.py
@@ -1,14 +1,14 @@
+# task 4
+
 from tensorflow.keras.models import Sequential
 from keras.layers import Dense
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, f1_score
 import pandas as pd
 import tensorflow as tf
-import numpy as np
 from sklearn.model_selection import GridSearchCV
 from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
 
-df = pd.read_csv('data/covtype.data', delimiter=',', header=None)
+df = pd.read_csv('./InternshipTaskRestAPI/data/covtype.data', delimiter=',', header=None)
 
 X = df.iloc[:, :-1]
 y = df.iloc[:, -1] - 1
@@ -18,7 +18,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=123, test
 classes = len(y.unique())
 
 
-def create_model(units1=100, units2=20, lr=0.001):
+def create_model(units1=160, units2=20, lr=0.001):
     model = Sequential([
         Dense(units1, activation='relu'),
         Dense(units2, activation='relu'),
@@ -32,12 +32,12 @@ def create_model(units1=100, units2=20, lr=0.001):
 
 
 param_grid = {
-    'units1': [50, 100, 150],
-    'units2': [10, 20, 30],
+    'units1': [20, 40, 160],
+    'units2': [10, 20, 40],
     'lr': [0.001, 0.01, 0.1]
 }
 
-model = KerasClassifier(build_fn=create_model, epochs=4, verbose=0)
+model = KerasClassifier(build_fn=create_model, epochs=20, verbose=0)
 
 grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=3, n_jobs=-1)
 grid_result = grid_search.fit(X_train, y_train)
